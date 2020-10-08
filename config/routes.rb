f1ce5fcf353b8949c76b89346bc2016172723347
Mehedi Ehteshum <me69oshan@gmail.com>
2020-10-08 00:44:33 -0600

    Test page added;

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  # route to 'controller#action'.
+  root to: 'pages#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
